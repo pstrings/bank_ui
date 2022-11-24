d7919ec9de0819a94fae49f412a4286b4acb6c4a
Suyash <paradisestrings99@gmail.com>
2022-11-24 17:46:41 +0530

    Created structure and component files

---
+++ src/components/CTA.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const CTA = () => <div></div>;
+
+export default CTA;
---
+++ src/components/Clients.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Clients = () => <div></div>;
+
+export default Clients;
