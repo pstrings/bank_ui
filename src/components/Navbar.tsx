a6a69868216c60d1eea7d9bd432e5941ecf2d540
Suyash <paradisestrings99@gmail.com>
2022-11-24 20:17:39 +0530

    navbar created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,61 @@
 import React from "react";
 
-const Navbar = () => <div></div>;
+import { useState } from "react";
+
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../static_data/static_data";
+
+const Navbar = () => {
+	const [navToggle, setNavToggle] = useState(false);
+
+	return (
+		<nav className="w-full flex py-6 justify-between items-center navbar">
+			<img src={logo} alt="bank" className="w-[124px] h-[32px]" />
+
+			<ul className="list-none sm:flex hidden justify-end items-center flex-1">
+				{navLinks.map((nav, index) => (
+					<li
+						key={nav.id}
+						className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+							index === navLinks.length - 1 ? "mr-0" : "mr-10"
+						}`}
+					>
+						<a href={`#${nav.id}`}>{nav.title}</a>
+					</li>
+				))}
+			</ul>
+
+			<div className="sm:hidden flex flex-1 justify-end items-center">
+				<img
+					src={navToggle ? close : menu}
+					alt="menu"
+					className="w-[28px] h-[28px] object-contain"
+					onClick={() => {
+						setNavToggle((prev) => !prev);
+					}}
+				/>
+
+				<div
+					className={`${
+						navToggle ? "flex" : "hidden"
+					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+				>
+					<ul className="list-none flex flex-col justify-end items-center flex-1">
+						{navLinks.map((nav, index) => (
+							<li
+								key={nav.id}
+								className={`font-poppins font-normal cursor-pointer text-[16px] text-white ${
+									index === navLinks.length - 1 ? "mr-0" : "mb-4"
+								}`}
+							>
+								<a href={`#${nav.id}`}>{nav.title}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
